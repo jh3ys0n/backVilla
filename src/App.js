12fc7850dcb8187f465e889e00c69ae17a99e6bf
jh3ys0n <gazelaldena@gmail.com>
2024-09-07 22:07:59 -0400

    mejoras

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ import About_us from "./pages/About_us";
 import Pages404 from "./pages/Pages404";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes >
-        <Route path="/" element={<CommonPage><Home /></CommonPage>} />
-        <Route path="/login" element={<CommonPage><Login /></CommonPage>} />
-        <Route path="/news" element={<CommonPage><New></New></CommonPage>} />
-        <Route path="/list_news" element={<CommonPage><ListNew></ListNew></CommonPage>} />
-        <Route path="/about" element={<CommonPage><About_us></About_us>  </CommonPage>} />
-        <Route path="/404" element={<CommonPage> <Pages404></Pages404> </CommonPage>} />
-        <Route path="/test" element={<TestPage></TestPage>} />
-      </Routes>
-    </BrowserRouter>
+    <Home></Home>
   );
 }
 
